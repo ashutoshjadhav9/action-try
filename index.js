58359d3e5ff9af3c55a7d83c6b399cf492c14b79
ashutoshjadhav9 <Ashutosh.Jadhav2@ibm.com>
2023-12-17 19:34:42 +0400

    New-Files-Actions

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const core = require('@actions/core');
-const exec = require('@actions/exec');
+const github = require('@actions/github');
 
-async function run() {
-	try {
-		const versionType = core.getInput('versions-type');
-		const src = __dirname;
-
-		await exec.exec(`${src}/git_update.sh -v ${versionType}`);
-	} catch (error) {
-		core.setFailed(error.message);
-	}
+try {
+  // `who-to-greet` input defined in action metadata file
+  const nameToGreet = core.getInput('who-to-greet');
+  console.log(`Hello ${nameToGreet}!`);
+  const time = (new Date()).toTimeString();
+  core.setOutput("time", time);
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
+} catch (error) {
+  core.setFailed(error.message);
 }
 
-run();
